664a5d69f86c704eb3f783fd1df289a6a6257d50
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 01:07:16 -0300

    feat: add CadastroModule to App and create a route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastroModule } from '../presentation/components/cadastro/cadastro.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CadastroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
