59af64829a41c1380ee50c6be9a8ff8bb41faa6b
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 01:05:55 -0300

    feat: create documentacao component

---
+++ src/presentation/components/cadastro/documentacao/documentacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentacao',
+  templateUrl: './documentacao.component.html',
+  styleUrl: './documentacao.component.css'
+})
+export class DocumentacaoComponent {
+
+}
