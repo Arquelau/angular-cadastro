dffb2c1c65de5f61ae6ef3a73a3ef7831e67e3b7
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 01:04:37 -0300

    feat: create informacoes pessoais component

---
+++ src/presentation/components/cadastro/informacoes-pessoais/informacoes-pessoais.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informacoes-pessoais',
+  templateUrl: './informacoes-pessoais.component.html',
+  styleUrl: './informacoes-pessoais.component.css'
+})
+export class InformacoesPessoaisComponent {
+
+}
