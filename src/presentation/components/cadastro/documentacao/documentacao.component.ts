6b04883a09113858102100a2312a192f648611ee
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 21:15:14 -0300

    feat: make documentacao work properly

--- src/presentation/components/cadastro/documentacao/documentacao.component.ts
+++ src/presentation/components/cadastro/documentacao/documentacao.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core'
+import { FormControl } from '@angular/forms'
+import { CpfPipe } from './cpf.pipe'
 
 @Component({
   selector: 'app-documentacao',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrl: './documentacao.component.css'
 })
 export class DocumentacaoComponent {
+  @Input() buttonTrigger: boolean | undefined
+  buttonToggle: boolean = false
+  rg: FormControl
+  passaporte: FormControl
+  cnh: FormControl
+  cpf: FormControl
+  cpfPipe: CpfPipe
 
+  refresh: number
+  localStorageName: string
+
+  constructor(){
+    this.cpf = new FormControl()
+    this.cpfPipe = new CpfPipe()
+    this.rg = new FormControl()
+    this.passaporte = new FormControl()
+    this.cnh = new FormControl()
+    this.refresh = 0
+    this.localStorageName = 'documentacao'
+    this.buttonToggle = false
+  }
+
+  transformCpf(value: string){
+    this.cpf.setValue(this.cpfPipe.transform(value))
+  }
+
+  ngOnInit() {
+    const localData = localStorage.getItem(this.localStorageName)
+    let jsonLocalData
+    if(localData != null){
+      jsonLocalData = JSON.parse(localData)
+    }
+    if (localData?.length){
+      this.rg.setValue(jsonLocalData.rg)
+      this.cpf.setValue(jsonLocalData.cpf)
+      this.passaporte.setValue(jsonLocalData.passaporte)
+      this.cnh.setValue(jsonLocalData.cnh)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['buttonTrigger'] && changes['buttonTrigger']?.previousValue != changes['buttonTrigger']?.currentValue && this.refresh >= 1) {
+      this.buttonToggle= !this.buttonToggle
+
+      let data = {
+        rg: this.rg.getRawValue(),
+        cpf: this.cpf.getRawValue(),
+        passaporte: this.passaporte.getRawValue(),
+        cnh: this.cnh.getRawValue()
+      }
+
+      localStorage.setItem(this.localStorageName, JSON.stringify(data))
+    }
+    this.refresh++
+  }
 }
