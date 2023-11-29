2fd2f2ed5894625298eb26bbced3a36ec29fe144
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 21:14:05 -0300

    feat: make emprego e educacao work properley

--- src/presentation/components/cadastro/emprego-educacao/emprego-educacao.component.ts
+++ src/presentation/components/cadastro/emprego-educacao/emprego-educacao.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-emprego-educacao',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './emprego-educacao.component.css'
 })
 export class EmpregoEducacaoComponent {
+  @Input() buttonTrigger: any
+  buttonToggle: boolean = false
+  refresh: number
 
+  renda: FormControl
+  historicoDeCredito: FormControl
+  contaBancaria: FormControl
+
+  localStorageName: string
+
+  constructor(){
+    this.renda = new FormControl()
+    this.historicoDeCredito = new FormControl()
+    this.contaBancaria = new FormControl()
+    this.buttonToggle = false
+    this.localStorageName = 'empregoEducacao'
+    this.refresh = 0
+  }
+
+  ngOnInit() {
+    const localData = localStorage.getItem(this.localStorageName)
+    let jsonLocalData
+    if(localData != null){
+      jsonLocalData = JSON.parse(localData)
+    }
+    if (localData?.length){
+      this.renda.setValue(jsonLocalData.renda)
+      this.historicoDeCredito.setValue(jsonLocalData.historicoDeCredito)
+      this.contaBancaria.setValue(jsonLocalData.contaBancaria)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['buttonTrigger'] && changes['buttonTrigger']?.previousValue != changes['buttonTrigger']?.currentValue && this.refresh >= 1) {
+      this.buttonToggle= !this.buttonToggle
+
+      let data = {
+      renda: this.renda.getRawValue(),
+      historicoDeCredito: this.historicoDeCredito.getRawValue(),
+      contaBancaria: this.contaBancaria.getRawValue()
+      }
+
+      localStorage.setItem(this.localStorageName, JSON.stringify(data))
+    }
+    this.refresh++
+  }
 }
