4cc39ca8afb6b8eed9c35e5149fb0a6a6a361395
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 21:14:37 -0300

    feat: make emergencia work properly

--- src/presentation/components/cadastro/emergencia/emergencia.component.ts
+++ src/presentation/components/cadastro/emergencia/emergencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-emergencia',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './emergencia.component.css'
 })
 export class EmergenciaComponent {
+  @Input() buttonTrigger: boolean | undefined
+  buttonToggle: boolean = false
+  contatoDeEmergencia: FormControl
+  informacoesMedicasRelevantes: FormControl
+  refresh: number
+  localStorageName: string
 
+  constructor(){
+    this.buttonToggle = false
+    this.contatoDeEmergencia = new FormControl()
+    this.informacoesMedicasRelevantes = new FormControl()
+    this.refresh = 0
+    this.localStorageName = 'emergencia'
+  }
+
+  ngOnInit() {
+    const localData = localStorage.getItem(this.localStorageName)
+    let jsonLocalData
+    if(localData != null){
+      jsonLocalData = JSON.parse(localData)
+    }
+    if (localData?.length){
+      this.contatoDeEmergencia.setValue(jsonLocalData.contatoDeEmergencia)
+      this.informacoesMedicasRelevantes.setValue(jsonLocalData.informacoesMedicasRelevantes)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['buttonTrigger'] && changes['buttonTrigger']?.previousValue != changes['buttonTrigger']?.currentValue && this.refresh >= 1) {
+      this.buttonToggle= !this.buttonToggle
+
+      let data = {
+      contatoDeEmergencia: this.contatoDeEmergencia.getRawValue(),
+      informacoesMedicasRelevantes: this.informacoesMedicasRelevantes.getRawValue()
+      }
+
+      localStorage.setItem(this.localStorageName, JSON.stringify(data))
+    }
+    this.refresh++
+  }
 }
