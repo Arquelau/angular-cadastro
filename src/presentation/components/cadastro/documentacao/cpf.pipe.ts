8c37f58827ea2a2befdc0c3f2c6dd0bcd06051e7
Felipe Arquelau <felipefatp2.0@gmail.com>
2023-11-28 21:15:33 -0300

    feat: add cpf pipe to change cpf format

---
+++ src/presentation/components/cadastro/documentacao/cpf.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({ name: 'cpf' })
+export class CpfPipe implements PipeTransform {
+    transform(value: string|number): string {
+      const cpfLenght = 11
+      if(value.toString().length != cpfLenght){
+        return value.toString()
+      }
+      let formattedValue = value + ''
+      formattedValue = formattedValue
+          .padStart(11, '0')                  
+          .substring(0, 11)                      
+          .replace(/[^0-9]/, '')              
+          .replace(                           
+              /(\d{3})(\d{3})(\d{3})(\d{2})/,
+              '$1.$2.$3-$4'
+          );
+
+      return formattedValue
+    }
+}
